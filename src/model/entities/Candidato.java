6dbf67c6f1a58e53ad9b6bc89eda355ce0fcf11d
Junior <Junior@192.168.1.23>
2020-11-17 16:02:27 -0300

    inserido atribudos do candidato

--- src/model/entities/Candidato.java
+++ src/model/entities/Candidato.java
@@ -1,18 +1,63 @@
 package model.entities;
 
-public class Candidato {
+public class Candidato  extends Usuario2{
 
+	private String DataNascimento;
+	private int Telefone;
+	private String Endereco;
+	private boolean Deficiencia;
+	private int Nota;
 	private int prioridade;
-
 	
-	
-	
+	public String getDataNascimento() {
+		return DataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		DataNascimento = dataNascimento;
+	}
+	public int getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(int telefone) {
+		Telefone = telefone;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	public boolean isDeficiencia() {
+		return Deficiencia;
+	}
+	public void setDeficiencia(boolean deficiencia) {
+		Deficiencia = deficiencia;
+	}
+	public int getNota() {
+		return Nota;
+	}
+	public void setNota(int nota) {
+		Nota = nota;
+	}
 	public int getPrioridade() {
 		return prioridade;
 	}
-
 	public void setPrioridade(int prioridade) {
 		this.prioridade = prioridade;
 	}
+	@Override
+	public String toString() {
+		return  "DataNascimento=" + DataNascimento + ", Telefone=" + Telefone + ", Endereco=" + Endereco
+				+ ", Deficiencia=" + Deficiencia + ", Nota=" + Nota;
+	}
+	@Override
+	public void visualisarEdital() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
 	
 }
--- src/model/services/CandidatoService.java
+++ src/model/services/CandidatoService.java
@@ -1,6 +1,9 @@
 package model.services;
 
+import javax.swing.JOptionPane;
+
 import model.entities.Candidato;
+
 import util.LinkedList;
 
 public class CandidatoService {
@@ -9,4 +12,28 @@ public class CandidatoService {
 		return null;
 	}
 	
+	public void RealizaInscricao () {
+		int D;
+		
+		Candidato user = new Candidato ();
+		user.setDataNascimento(JOptionPane.showInputDialog("Digite data de Nascimento DD/MM/AAAA"));
+		user.setTelefone(Integer.parseInt(JOptionPane.showInputDialog("Digite telefone")));
+		user.setEndereco(JOptionPane.showInputDialog("Digite endereço"));
+		D =  Integer.parseInt(JOptionPane.showInputDialog("Se possuir alguma deficiencia Digite 1 caso contrario 2"));
+		if (D == 1) {
+			user.setDeficiencia(true);
+		}else if(D==2) {
+			user.setDeficiencia(false);
+		}
+		 
+		System.out.println(user.getNome() + "  " +user);
+	}
+	
+	public void acompanhaInscricao() {
+		
+	}
+	
+	public void solicitaRecurso() {
+		
+	}
 }
